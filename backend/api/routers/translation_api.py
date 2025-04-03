29c99525e16c882d52a16ed5b2fed0cf76c994cf
vunt199802-coder <vunt199802@gmail.com>
2025-04-03 14:09:55 +0300

    use the new model for the translation endpoint

--- backend/api/routers/translation_api.py
+++ backend/api/routers/translation_api.py
@@ -1,17 +1,13 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from models.api_models import TranslationRequest
 from translation import translate_query
 router = APIRouter()
-
-class TranslationQuery(BaseModel):
-    query:str
-    translation_language:str
 
 @router.get("/")
 def root():
     return {"hello":"world"}
 
 @router.post("/translate")
-async def translate(translation_query:TranslationQuery):
+async def translate(translation_query:TranslationRequest):
     response = translate_query(translation_query.query, translation_query.translation_language)
-    return response
+    return {"original_query":translation_query.query, "translated_query":response}
