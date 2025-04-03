bec4d398c9936cafbc7ee7ec136206d23d5ec635
vunt199802-coder <vunt199802@gmail.com>
2025-04-03 14:05:35 +0300

    refactor the api to have a router

--- backend/api/routers/translation_api.py
+++ backend/api/routers/translation_api.py
@@ -1,18 +1,17 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 from pydantic import BaseModel
 from translation import translate_query
+router = APIRouter()
 
 class TranslationQuery(BaseModel):
     query:str
     translation_language:str
 
-app = FastAPI()
-
-@app.get("/")
+@router.get("/")
 def root():
     return {"hello":"world"}
 
-@app.post("/translate")
+@router.post("/translate")
 async def translate(translation_query:TranslationQuery):
     response = translate_query(translation_query.query, translation_query.translation_language)
     return response
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
 import uvicorn
+from fastapi import FastAPI
+from api.routers import translation_api
+
+app = FastAPI()
+
+app.include_router(translation_api.router)
 
 if __name__ == "__main__":
-    uvicorn.run("api.translation_api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
