adbbf6064c2b998eb87d1ecc537daf69a2b38d21
vunt199802-coder <vunt199802@gmail.com>
2025-04-03 14:37:58 +0300

    cleanup

--- backend/api/routers/translation_api.py
+++ backend/api/routers/translation_api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
+from fastapi import Depends
 from models.api_models import TranslationRequest
-from translation import translate_query
+from services.translation import Translator
+from services.setup import get_translator
 router = APIRouter()
 
 @router.get("/")
@@ -8,6 +10,6 @@ def root():
     return {"hello":"world"}
 
 @router.post("/translate")
-async def translate(translation_query:TranslationRequest):
-    response = translate_query(translation_query.query, translation_query.translation_language)
+async def translate(translation_query:TranslationRequest, translation_service:Translator=Depends(get_translator)):
+    response = translation_service.translate(translation_query.query, translation_query.target_language)
     return {"original_query":translation_query.query, "translated_query":response}
--- backend/models/api_models.py
+++ backend/models/api_models.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel
 class TranslationRequest(BaseModel):
     "Request model for the translation api"
     query:str
-    translation_language:str
+    target_language:str
